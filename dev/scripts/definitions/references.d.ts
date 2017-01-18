ea5c3a86c49e6fa9eb352b08bc7e79be5a37e3ac
mac <mac@impact.dk>
2017-01-18 17:21:19 +0100

    Simple solution to load order. + Source maps from grunt-ts are now inline.

--- dev/scripts/definitions/references.d.ts
+++ dev/scripts/definitions/references.d.ts
@@ -2,9 +2,11 @@
 /// <reference path="../modules/example/templates/example.html.ts" />
 /// <reference path="../app.config.ts" />
 /// <reference path="../app.run.ts" />
-/// <reference path="../app.ts" />
 /// <reference path="node_modules/@types/angular/index.d.ts" />
 /// <reference path="node_modules/@types/jquery/index.d.ts" />
 /// <reference path="../modules/example/example.module.ts" />
 //grunt-end
 
+// Main app file should be at the bottom of the load order.
+// Ensures that bootstrap only happens after all other js has been parsed.
+/// <reference path="../app.ts" />
